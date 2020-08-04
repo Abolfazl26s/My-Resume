3fd7e98cb097b5474f74d75e7ec63be984e1a4e3
Abolfazl26s <asaeidabadi243@gmail.com>
2020-08-05 00:27:33 +0430

    Updates

---
+++ precache-manifest.b3320544a4acfb28573b17d37ce3dbda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7e41be1d8eb84ac22e72d0aa1e23b5",
+    "url": "/My-Resume/index.html"
+  },
+  {
+    "revision": "12321c14b1bc3e8a16b6",
+    "url": "/My-Resume/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "00373a4f93e4a7ff3dcb",
+    "url": "/My-Resume/static/css/main.97e20ec1.chunk.css"
+  },
+  {
+    "revision": "12321c14b1bc3e8a16b6",
+    "url": "/My-Resume/static/js/2.e9ecb908.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/My-Resume/static/js/2.e9ecb908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00373a4f93e4a7ff3dcb",
+    "url": "/My-Resume/static/js/main.8cdd975d.chunk.js"
+  },
+  {
+    "revision": "669a0678b8cc2ed784cc",
+    "url": "/My-Resume/static/js/runtime-main.6a366704.js"
+  }
+]);
